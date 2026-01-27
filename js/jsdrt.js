2f6a7472dda172f8966a2b0da146537f430c8ed7
Robert Leonhardt <ohmy@pm.me>
2026-01-27 19:48:15 +0100

    Initialize PyDRT implementation

---
+++ js/jsdrt.js
@@ -0,0 +1,26 @@
+/**
+ * JavaScript implementation of PyDRT
+ * (https://github.com/robertleonhardt/PyDRT)
+ */
+
+// const Matrix = mlMatrix.Matrix; // already handled by the nnls solver
+
+class DRT {
+    constructor(frequencies, zReal, zImag, options = {}) {
+        // // Main data
+        // this.frequencies = frequencies;
+        // this.zReal = zReal;
+        // this.zImag = zImag;
+
+        // // Options with defaults
+        // this.tauMin = options.tauMin ?? 1e-7;
+        // this.tauMax = options.tauMax ?? 1e3;
+        // this.tauPoints = options.tauPoints ?? 100;
+        // this.epsilon = options.epsilon ?? 0; // Regularization, we typically don't want that
+        // this.solvingData = options.solvingData ?? 'both'; // 'real', 'imag', 'both'
+
+        // // Generate tau array
+        // // this.taus
+    }
+}
+
